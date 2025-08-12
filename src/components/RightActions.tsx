34d40cf35a94068b3ea9e072eda722d2046581ad
CodieMisiani <nimrodmisiani1@gmail.com>
2025-08-12 22:26:21 +0300

    feat(layout): refactor RightActions component for improved button structure and add cart item count badge

--- src/components/RightActions.tsx
+++ src/components/RightActions.tsx
@@ -17,24 +17,42 @@ export default function RightActions() {
   return (
     <div className="flex items-center space-x-4">
       {/* Search */}
-      <Button variant={"ghost"} size={"icon"} className="hidden md:flex">
+      <Button
+        variant={"ghost"}
+        size={"icon"}
+        className="hidden md:flex  hover:text-green-600 transition-colors duration-300 cursor-pointer"
+      >
         <SearchIcon className="w-5 h-5" />
       </Button>
       {/* Wishlist */}
-      <Button variant={"ghost"} size={"icon"}>
+      <Button
+        variant={"ghost"}
+        size={"icon"}
+        className="  hover:text-green-600 transition-colors duration-300 cursor-pointer"
+      >
         <Heart className="w-5 h-5" />
       </Button>
       {/* User */}
-      <Button variant={"ghost"} size={"icon"}>
+      <Button
+        variant={"ghost"}
+        size={"icon"}
+        className="cursor-pointer  hover:text-green-600 transition-colors duration-300"
+      >
         <User className="w-5 h-5" />
       </Button>
       {/* Cart */}
-      <Button variant={"ghost"} size={"icon"}>
+      <Button
+        variant={"ghost"}
+        size={"icon"}
+        className=" relative  hover:text-green-600 transition-colors duration-300 cursor-pointer"
+      >
         <ShoppingBag className="w-5 h-5" />
         <Badge
           variant={"destructive"}
           className="absolute -top-2 -right-2 h-5 w-5 flex items-center justify-center p-0 text-xs"
-        ></Badge>
+        >
+          {cartItems}
+        </Badge>
       </Button>
       {/* RightActions */}
     </div>
